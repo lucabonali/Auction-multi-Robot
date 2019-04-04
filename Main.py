a514f77f242a14ba828abe72a778e04e8c390f00
Luca <lucabonali12@gmail.com>
2019-04-04 19:43:08 +0200

    oid

--- Main.py
+++ Main.py
@@ -13,37 +13,7 @@ def startRouting(path):
         for j in auction.schedule.agents:
             j.resetState()
             #j.printState()
+    print(auction.schedule.agents[0].targets)
 
-'''
-    print(0 ,"^ STEP////////////////////////////////////////////")
-    auction.step()
-    print("\n\nSTATE OF AGENTS AT THE END OF ROUND 0\n\n\n")
-    for i in auction.schedule.agents:
-        i.resetState()
-        i.printState()
-    print(0, "^END STEP////////////////////////////////////////////")
-    print(1, "^ STEP////////////////////////////////////////////")
-    print("\n\nSTATE OF AGENTS AT THE END OF ROUND 1\n\n\n")
-    auction.step()
-    for i in auction.schedule.agents:
-        i.resetState()
-        i.printState()
 
-    print(2, "^ STEP////////////////////////////////////////////")
-    auction.step()
-    print("\n\nSTATE OF AGENTS AT THE END OF ROUND 2\n\n\n")
-    for i in auction.schedule.agents:
-        i.resetState()
-        i.printState()
-    print(2, "^ STEP////////////////////////////////////////////")
-
-    print(3, "^ STEP////////////////////////////////////////////")
-    auction.step()
-    print("\n\nSTATE OF AGENTS AT THE END OF ROUND 3\n\n\n")
-    for i in auction.schedule.agents:
-        i.resetState()
-        i.printState()
-    print(3, "^ STEP////////////////////////////////////////////")
-
-'''
-startRouting("Map2.txt")
+startRouting("Map.txt")
